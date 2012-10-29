fcd161f5c17167e78cb2b9ca8294f7b6ae30d0c6
wazp <wazpys@gmail.com>
2012-10-29 06:52:43 -0500

    Password placeholder support
    Added support for placeholder on input type=password fields.

--- nInputDefault.js
+++ nInputDefault.js
@@ -1,5 +1,5 @@
-/* nInputDefault v0.5 by Patrick Waks, Nansen <patrick.waks@nansen.se>
- * 
+/*! nInputDefault - https://github.com/wazp/nInputDefault */
+/* 
  * Adds support for placeholder="" attribute on input type=text in non-HTML5
  * supported browsers.
  * 
@@ -9,27 +9,27 @@
  * $('input').nInputDefault();
  * $('input.anotherStyle').nInputDefault({focusClass: 'myFocusClass'});
  */
-(function($){
-	$.fn.nInputDefault = function(userOptions) {
+;(function($){
+	$.fn.nInputDefault = function (userOptions) {
 		var defaults = { // default focus class applied to element, can be overridden in init
-			focusClass : 'focus'
+			focusClass: 'focus'
 		},
 		options = $.extend(defaults, userOptions),
 		// quick check to see if the browser can handle the placeholder attribute natively or not
-		canHandlePlaceholder = (function() { var i = document.createElement('input'); return 'placeholder' in i;})();
-		return this.each(function() {
-			var obj = $(this),strDefault;
+		canHandlePlaceholder = (function () { var i = document.createElement('input'); return 'placeholder' in i; })();
+		return this.each(function () {
+			var obj = $(this), strDefault, fake;
+			// save the current placeholder text
+			strDefault = obj.attr('placeholder');
 			// we only want this on <input type="text" /> and <textarea>'s.
 			if (obj.attr('type') == "text" || obj.is('textarea')) {
-				// save the current placeholder text
-				strDefault = obj.attr('placeholder');
 				// if no native support, add placeholder as value if not set already from POST
-				if(!canHandlePlaceholder && !obj.val().length) obj.val(strDefault);
+				if (!canHandlePlaceholder && !obj.val().length) obj.val(strDefault);
 				// bind event for focus
-				obj.unbind('focus.nInputDefault').bind('focus.nInputDefault', function(){
-					if(!canHandlePlaceholder){ // if no native support
+				obj.unbind('focus.nInputDefault').bind('focus.nInputDefault', function () {
+					if (!canHandlePlaceholder) { // if no native support
 						// check if not using default text or being empty already
-						if ((obj.val() == strDefault || obj.val() == '')) { 
+						if ((obj.val() == strDefault || obj.val() == '')) {
 							// remove value when clicked. Presto!
 							obj.val('');
 						}
@@ -37,8 +37,8 @@
 					// add the focus class we want, either default or user set.
 					obj.addClass(options.focusClass);
 
-				}).unbind('blur.nInputDefault').bind('blur.nInputDefault', function(){ // bind blur event
-					if(!canHandlePlaceholder){ // if no native support
+				}).unbind('blur.nInputDefault').bind('blur.nInputDefault', function () { // bind blur event
+					if (!canHandlePlaceholder) { // if no native support
 						// if not placeholder text or empty
 						if (obj.val() == strDefault || obj.val() == '') {
 							// put back the placeholder text as value
@@ -46,8 +46,32 @@
 						}
 					}
 					// remove the focus class, either standard or user set.
-					obj.removeClass(options.focusClass);	
+					obj.removeClass(options.focusClass);
 				});
+			} else if (obj.attr('type') == 'password') {
+				if (!canHandlePlaceholder) {
+					// make new fake password box as input text...
+					fake = $('<input type="text" id="' + obj.attr('id') + 'Fake" />').insertBefore(obj);
+					obj.hide();
+					// if no native support, add placeholder as value if not set already from POST
+					fake.val(strDefault);
+
+					$(fake).unbind('focus.nInputDefault').bind('focus.nInputDefault', function (e) {
+						if (!canHandlePlaceholder || obj.val() == '') {
+							fake.hide();
+							obj.val('').show().focus();
+						}
+					})
+
+					$(obj).unbind('blur.nInputDefault').bind('blur.nInputDefault', function () {
+						if (!canHandlePlaceholder) {
+							if (obj.val() == strDefault || obj.val() == '') {
+								obj.hide();
+								fake.show();
+							}
+						}
+					});
+				}
 			}
 		});
 	};
